a06a8a98d147dd6850751f0ad8f267be37ff0c1d
Oskar <oskarpuchalski17@gmail.com>
2021-06-16 16:49:41 +0200

    Changed crypto logos. Styled moblie view

--- public/app.js
+++ public/app.js
@@ -1,27 +1,44 @@
 const tableBody = document.querySelector("#table--body");
 
-const cryptoKeys = ["btc", "eth", "doge", "usdt", "xrp", "dot"];
+const cryptoKeys = [
+  "btc",
+  "eth",
+  "doge",
+  "usdt",
+  "xrp",
+  "dot",
+  "ada",
+  "uni",
+  "ltc",
+  "bch",
+  "sol",
+  "link",
+  "matic",
+  "theta",
+];
 
 const getCryptoInfo = async (key) => {
-  const response = await fetch(
-    `https://api.cryptonator.com/api/ticker/${key}-usd`
-  );
-  const crypto = await response.json();
-  return crypto;
+  let apiKey = `https://api.cryptonator.com/api/ticker/${key}-usd`;
+  const response = await fetch(apiKey);
+  return await response.json();
 };
 
-const updateCryptoInfo = () => {};
-for (let i = 0; i < cryptoKeys.length; i++) {
-  getCryptoInfo(cryptoKeys[i]).then((data) => {
-    console.log(cryptoKeys[i]);
-    let { base, price, change } = data.ticker;
-    let html = `
-                <td scope="row"><img class="crypto--img" src="images/logo/${base.toLowerCase()}.png"></img>${base}</td>
-                <td>$${price}</td>
-                <td>${change}%</td>
+const updateCryptoInfo = () => {
+  for (let i = 0; i < 10; i++) {
+    getCryptoInfo(cryptoKeys[i]).then((data) => {
+      let { base, price, change } = data.ticker;
+      let logoSrc = `http:\\node_modules/cryptocurrency-icons/32/color/${base.toLowerCase()}.png`;
+      let html = `
+                <td class="table--td table--name" scope="row"><img class="crypto--img" src=${logoSrc} ></img>${base}</td>
+                <td class="table--td table--price">$${price}</td>
+                <td class="table--td table--change">${change}%</td>
             `;
-    tr = document.createElement("tr");
-    tr.innerHTML = html;
-    tableBody.appendChild(tr);
-  });
-}
+      tr = document.createElement("tr");
+      tr.classList.add("table--row");
+      tr.innerHTML = html;
+      tableBody.appendChild(tr);
+    });
+  }
+};
+
+updateCryptoInfo();
