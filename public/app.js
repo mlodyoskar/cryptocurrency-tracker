1a405461945e3bc33b2b7343097b99738107d0e6
BuildTools <unconfigured@null.spigotmc.org>
2021-06-30 08:42:34 +0200

    Solved problem with sorting cryptocurrenices before rendering them

--- public/app.js
+++ public/app.js
@@ -23,22 +23,37 @@ const getCrypto = async (key) => {
   return await response.json();
 };
 
-const renderCrypto = () => {
-  for (let i = 0; i < 10; i++) {
-    getCrypto(cryptoKeys[i]).then((data) => {
-      let { base, price, change } = data.ticker;
-      let logoSrc = `http:\\node_modules/cryptocurrency-icons/32/color/${base.toLowerCase()}.png`;
-      let html = `
+const storeCrypto = async () => {
+  promises = [];
+  cryptos = [];
+  for (let e of cryptoKeys) {
+    promises.push(getCrypto(e));
+  }
+  Promise.all(promises).then((v) => {
+    v.forEach((e) => {
+      cryptos.push(e.ticker);
+    });
+    cryptos.sort((a, b) => {
+      return b.price - a.price;
+    });
+  });
+  return cryptos;
+};
+
+const renderCrypto = async () => {
+  storeCrypto().then((data) => {
+    console.log(data);
+    let logoSrc = `http:\\node_modules/cryptocurrency-icons/32/color/${base.toLowerCase()}.png`;
+    let html = `
                 <td class="table--td table--name" scope="row"><img class="crypto--img" src=${logoSrc} ></img>${base}</td>
                 <td class="table--td table--price">$${price.slice(0, 12)}</td>
                 <td class="table--td table--change">${change}%</td>
             `;
-      tr = document.createElement("tr");
-      tr.classList.add("table--row");
-      tr.innerHTML = html;
-      tableBody.appendChild(tr);
-    });
-  }
+    tr = document.createElement("tr");
+    tr.classList.add("table--row");
+    tr.innerHTML = html;
+    tableBody.appendChild(tr);
+  });
 };
 
 renderCrypto();
